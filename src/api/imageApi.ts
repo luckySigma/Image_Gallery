1a2fdf16be5553062c9a04706c6c6123c62d9db3
luckySigma <adsanjeewa37@gmail.com>
2026-07-06 19:02:18 +0530

    feat: integrate frontend with updated image API (#3)

--- src/api/imageApi.ts
+++ src/api/imageApi.ts
@@ -6,20 +6,12 @@ export const IMAGES_PER_PAGE = 12;
 export const imageApi = createApi({
   reducerPath: "imageApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.unsplash.com/",
-    prepareHeaders: (headers) => {
-      headers.set(
-        "Authorization",
-        `Client-ID ${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`,
-      );
-
-      return headers;
-    },
+    baseUrl: "https://gallery-api-lf4c.onrender.com/api/images",
   }),
   endpoints: (builder) => ({
     getImages: builder.query<UnsplashImage[], number>({
       query: (page = 1) => {
-        return `photos?page=${page}&per_page=${IMAGES_PER_PAGE}&order_by=popular`;
+        return `?page=${page}&per_page=${IMAGES_PER_PAGE}&order_by=popular`;
       },
     }),
 
